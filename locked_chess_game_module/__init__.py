baa4f3a0e23d8187dadbb64f28c553884caf7d9f
Locked-chess-official <13140752715@163.com>
2024-12-28 09:07:22 +0800

    Update __init__.py

--- locked_chess_game_module/__init__.py
+++ locked_chess_game_module/__init__.py
@@ -2,5 +2,7 @@ import sys
 
 major_version=sys.version_info.major
 minor_version=sys.version_info.minor
-if not (major_version>=3 and minor_version>=8):
+if not (major_version>=3):
+    raise ImportError('The version is below 3.8 .')
+elif (major_version==3 and minor_version<8):
     raise ImportError('The version is below 3.8 .')
