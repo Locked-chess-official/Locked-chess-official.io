17f0089d5f9abbc190250be614b6ffe0558e3f4a
Locked-chess-official <13140752715@163.com>
2025-01-02 09:30:13 +0800

    Update __init__.py

--- locked_chess_game_module/__init__.py
+++ locked_chess_game_module/__init__.py
@@ -1,4 +1,6 @@
 import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from locked_chess_game_module import *
 from locked_chess_game_class import LockedChessClass
 
